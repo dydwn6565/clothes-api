3157db9920bba896a3641d1982a408acf0074e32
YongjuLee6565 <ivan.yongju.lee@gmail.com>
2022-02-15 04:59:25 -0800

    added end point(hoody,cardigan,denim pants, slacks

--- api/items.js
+++ api/items.js
@@ -11,4 +11,40 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/hoody", async (req, res) => {
+  try {
+    res.json(items["outer"]["hoody"]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+router.get("/cardigan", async (req, res) => {
+  try {
+    res.json(items["outer"]["hoody"]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+router.get("/denim pants", async (req, res) => {
+  try {
+    res.json(items["pants"]["denim pants"]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+router.get("/slacks", async (req, res) => {
+  try {
+    res.json(items["pants"]["slacks"]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
