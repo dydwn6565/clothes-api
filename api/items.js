97bbb3c0e59328fdf0ad4df837aa8f8120041be2
YongjuLee6565 <ivan.yongju.lee@gmail.com>
2022-02-16 22:54:21 -0800

    added keyword in itemsjson file and working on search function

--- api/items.js
+++ api/items.js
@@ -75,4 +75,34 @@ router.get("/recommend", async (req, res) => {
   }
 });
 
+router.get("/searched", async (req, res) => {
+  try {
+    // res.json(items["recommend"]);
+    const keywords = req.query.keyword.split(",");
+    // console.log(items.outer.hoody[0].keyword.includes("gray"));
+    const matchedItemFromHoody = items.outer.hoody.filter((hoody) =>
+      hoody.keyword.includes(...keywords)
+    );
+
+    const matchedItemFromCardigan = items.outer.cardigan.filter((cardigan) =>
+      cardigan.keyword.includes(...keywords)
+    );
+    const matchedItemFromDenim = items.pants["denim pants"].filter((denim) =>
+      denim.keyword.includes(...keywords)
+    );
+    const matchedItemFromSlack = items.slacks.hoody.filter((slacks) =>
+      slacks.keyword.includes(...keywords)
+    );
+
+    // console.log(items["outer"]["hoody"][0]);
+    // console.log(items["outer"]["cardigan"].length);
+    // console.log(items["pants"]["denim pants"].length);
+    // console.log(items["pants"]["slacks"].length);
+    res.status(200).json(items);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
